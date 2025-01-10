6127869a7d0cc45df9902cff053332a02bb65817
tigocode <tiago.dantas16@live.com>
2025-01-09 23:06:45 -0300

    :sparkles: feat: inclusao de nova funcao usando o verso http POST

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const { cadastrarLeads } = require('./service/services');
 
-app.get('/leads', async (req, res) => {
-  cadastrarLeads('Tiago', 'tiago@email.com')
+app.use(cors());
+app.use(express.json());
+
+app.post('/leads', async (req, res) => {
+  const { nome, email } = req.body;
+
+  await cadastrarLeads(nome, email);
+
+  res.status(204).send();
 });
 
 app.listen(3000, () => {
--- src/service/services.js
+++ src/service/services.js
@@ -4,7 +4,7 @@ const cadastrarLeads = async (nome, email) => {
     const conexao = await pool.getConnection();
 
     const resposta = await conexao.query(`INSERT INTO  leads (nome, email) VALUES ("${nome}", "${email}")`)
-    console.log(resposta);
+    
     conexao.release();
 
     return resposta;
